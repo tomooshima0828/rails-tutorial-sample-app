3422ed33377400d6d6adae507ffafec7e695f679
tomooshima0828 <tomo.oshima0828@gmail.com>
2021-03-02 00:50:28 +0900

    finish 07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
   end
+  
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users (+ params)
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to the sample app!"
+        # flash.each do |message_type => [:success], message => "Welcome to the sample app!"|
+        redirect_to @user
+      else
+        render 'new'
+      end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
