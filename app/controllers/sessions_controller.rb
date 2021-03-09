739e7ca96cd8d3126904f1aca859f12ade39edb8
tomooshima0828 <tomo.oshima0828@gmail.com>
2021-03-09 10:34:51 +0900

    Finish CP 09

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       flash[:success] = "Successfully logged in!!!"
       log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      # remember user
       redirect_to user
     else
       flash.now[:danger] = "Invalid login information/Login failed"
@@ -14,7 +16,7 @@ class SessionsController < ApplicationController
     
   end
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = "Successfully logged out!!!"
     redirect_to root_url
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    # トークンは :nameや:emailといった属性のようにデータベースに保存できないので、
+    # 仮の属性 :remember_token を作り、トークンをブラウザのcookiesに保存する。 self.remember_token
+  attr_accessor :remember_token
   before_save { self.email = self.email.downcase }
   validates :name,  presence: true, 
                       length: { maximum: 50 }
@@ -15,4 +18,35 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # ランダムな文字列を生成する => 
+  # ①-a ランダムな文字列を:remember_tokenとしてcookiesに保存する　①-b :user_idも暗号化されてcookiesに保存される
+  # ②ランダムな文字列はrememberメソッドで不可逆的にハッシュ化されて、:remember_digestとしてデータベースに保存される
+  # ①と②を照合することでsessionが維持される(remember me)
+  def User.new_token 
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token #セッターメソッドを呼び出すときには必ずselfを付ける
+    update_attribute(:remember_digest, User.digest(remember_token)) #上記以外はselfは省略可能
+    # selfを付けた場合 => self.update_attribute(:remember_digest, User.digest(self.remember_token))
+    # update_attributeはvalidationを経由せずにデータベース上の属性を更新させる。
+    # このrememberメソッドではremember_tokenが生成されてUser.digestでハッシュ化されるだけなのでvalidationは不要。 
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    self.update_attribute(:remember_digest, nil)
+  end
 end
+
+
+
